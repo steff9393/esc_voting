92b6eb4846a0b4322b5897c9c278074764c321ea
Steffen <steffen.pankratz@gmail.com>
2023-05-07 00:00:28 +0200

    Vers 1.2 routes separated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from flask import Flask
+from routes import *
 
 DB_URL = os.getenv("DB_URL")
 
